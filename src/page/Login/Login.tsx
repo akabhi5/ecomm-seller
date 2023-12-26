8a1e40ffc7a508a646be6e244436c1b259e5e243
Abhishek Kumar <abhishekk580@gmail.com>
2023-12-26 23:33:33 +0530

    login form

--- src/page/Login/Login.tsx
+++ src/page/Login/Login.tsx
@@ -1,5 +1,48 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  email: string;
+  password: string;
+};
+
 const Login = () => {
-  return <div>Login</div>;
+  const { register, handleSubmit, watch } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  return (
+    <div className="max-w-lg mx-auto ">
+      <div className="w-full">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text text-lg">Email</span>
+            </div>
+            <input
+              type="email"
+              placeholder="Password"
+              className="input input-bordered w-full"
+              {...register("email", { required: true })}
+            />
+          </label>
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text text-lg">Password</span>
+            </div>
+            <input
+              type="password"
+              placeholder="Password"
+              className="input input-bordered w-full"
+              {...register("password", { required: true })}
+            />
+          </label>
+          <div className="mt-4">
+            <button className="btn btn-neutral text-lg">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
