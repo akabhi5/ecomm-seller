6dca6732a56983616782bc591360e83fc607f58a
Abhishek Kumar <abhishekk580@gmail.com>
2023-12-26 23:01:38 +0530

    added react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import NavBar from "./components/NavBar/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Home from "./page/Home/Home";
+import NoPage from "./page/NoPage/NoPage";
+import Login from "./page/Login/Login";
+import Register from "./page/Register/Register";
 
 function App() {
   return (
     <div>
-      <NavBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
   return (
     <nav className="flex justify-between items-center h-20 shadow-md p-4">
       <div className="text-3xl font-bold">Ecomm Seller</div>
       <div className="flex space-x-4 text-lg">
-        <div>Login</div>
-        <div>Register</div>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
       </div>
     </nav>
   );
