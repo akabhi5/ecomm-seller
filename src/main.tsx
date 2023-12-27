1e497b53a5ad53aa84235d6e945d0c97e4516963
Abhishek Kumar <abhishekk580@gmail.com>
2023-12-27 22:36:08 +0530

    added react query

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,18 @@ import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </Provider>
     ,
   </React.StrictMode>
