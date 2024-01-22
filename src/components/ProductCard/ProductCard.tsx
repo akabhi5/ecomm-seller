efaba6eddf9a9c3e439075f2ff7db5cb0074caf4
Abhishek Kumar <abhishekk580@gmail.com>
2024-01-22 21:38:55 +0530

    Fixed product card

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -6,13 +6,13 @@ interface Props {
 
 const ProductCard = ({ product }: Props) => {
   return (
-    <div className="w-[220px] border hover:shadow-md">
+    <div className="hover:shadow-md max-w-[220px] h-full border">
       <img
-        className="w-[250px] h-[270px] object-fill"
+        className="w-fit max-h-[250px] min-w-[200px] min-h-[250px] object-cover"
         src={product.product_images[0].url}
         alt={product.name}
       />
-      <div className="text-center">
+      <div className="p-2 text-center">
         <div>{product.name}</div>
         <div className="text-sm text-slate-500">Price: ₹{product.price}</div>
       </div>
