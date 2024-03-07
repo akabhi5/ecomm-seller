ab5661e693420ae3e19d8eb7b6f0191b38bbe02e
Abhishek Kumar <abhishekk580@gmail.com>
2024-03-07 11:31:47 +0530

    backend tech added

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,6 +2,8 @@ import { FaReact } from "react-icons/fa";
 import {
   SiAxios,
   SiDaisyui,
+  SiDjango,
+  SiPostgresql,
   SiReacthookform,
   SiReactquery,
   SiReactrouter,
@@ -9,6 +11,7 @@ import {
   SiTailwindcss,
   SiVite,
 } from "react-icons/si";
+import { DiDjango } from "react-icons/di";
 
 const Footer = () => {
   return (
@@ -43,6 +46,20 @@ const Footer = () => {
           </div>
           <div className="tooltip" data-tip="DaisyUI">
             <SiDaisyui size={50} />
+          </div>
+        </div>
+
+        <p>Backend</p>
+
+        <div className="flex items-center space-x-2">
+          <div className="tooltip" data-tip="Django">
+            <DiDjango size={60} />
+          </div>
+          <div className="tooltip" data-tip="PostgreSQL">
+            <SiPostgresql size={40} />
+          </div>
+          <div className="tooltip" data-tip="Django REST Framework">
+            <SiDjango size={40} />
           </div>
         </div>
       </aside>
